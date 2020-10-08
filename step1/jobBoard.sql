6877a9759427f72c035784b74806fa1bbcc915ff
Tranchat <joffrey.tranchat@epitech.eu>
2020-10-08 09:18:25 +0200

    modification post followUp bdd

--- step1/jobBoard.sql
+++ step1/jobBoard.sql
@@ -1,8 +1,9 @@
 CREATE TABLE `Annonce` ( 
-`idAnnonce` int AUTO_INCREMENT, 
+`idAnnonce` int AUTO_INCREMENT,
+`idEntreprise` int,
+`idPersonne` int,
 `nom` varchar(50), 
 `description` varchar(255), 
-`idEntreprise` int, 
 `place` int,
 `salaires` int,
 `tempDeTravailParSemaine` int,
@@ -14,7 +15,7 @@ CREATE TABLE `Entreprise` (
 `idEntreprise` int AUTO_INCREMENT,
 `nomEntreprise` varchar(50),
 `localisation` varchar(150),
-`activites` varchar(255),
+`activite` varchar(255),
 primary key (idEntreprise)
 );
 
@@ -36,22 +37,21 @@ primary key (idPersonne)
 
 CREATE TABLE `Candidature` (
 `idCandidature` int AUTO_INCREMENT,
-`contenueMail` varchar(10000),
 `idAnnonce` int,
-`idDemandeur` int,
-`idDestinataire` int,
+`idCandidat` int,
+`contenuMail` varchar(10000),
 primary key (idCandidature)
 );
 
 ALTER TABLE `Annonce` ADD FOREIGN KEY (`idEntreprise`) REFERENCES `Entreprise`(idEntreprise);
 
+ALTER TABLE `Annonce` ADD FOREIGN KEY (`idPersonne`) REFERENCES `Personne`(idPersonne);
+
 ALTER TABLE `Candidature` ADD FOREIGN KEY (`idAnnonce`) REFERENCES `Annonce`(idAnnonce);
 
-ALTER TABLE `Candidature` ADD FOREIGN KEY (`idDemandeur`) REFERENCES `Personne`(idPersonne);
+ALTER TABLE `Candidature` ADD FOREIGN KEY (`idCandidat`) REFERENCES `Personne`(idPersonne);
 
-ALTER TABLE `Candidature` ADD FOREIGN KEY (`idDestinataire`) REFERENCES `Personne`(idPersonne);
-
-INSERT INTO Entreprise (nomEntreprise, localisation, activites)
+INSERT INTO Entreprise (nomEntreprise, localisation, activite)
 VALUES
 ('Epitech', '3 Place Paul Bec, 34000 Montpellier', 'Ecoles informatique'),
 ('Google', '38 avenue de lopéra', 'Moteur de recherche web plutôt célèbre'),
@@ -62,14 +62,14 @@ VALUES
 INSERT INTO Personne (nom, prenom, age, sexe, mail, telephone,identifiant, motDePasse, status, diplome, experience)
 VALUES
 ('Alban','Rochas','20','homme','alban.rochas@epitech.eu','0658795879','a.rochas','sparkyoto','demandeur','BTS, Bac','Contrat étudiant à Leroy Merlin'),
-('Joffrey','Tranchat','20','homme','joffrey.tranchat@epitech.eu','0768785945','j.tranchat','Prodyy','demandeur','BTS Bac','stage Grand delta habitat à avignon pendant 6 semaines au services informatique'),
-('Michel','Honteux','54','homme','michel.honteux@gmail.com','0769696969','m.honteux','honteux','demandeur',' Informatique','25 ans en freelance'),
-('Patrick','Balkany','47','homme','Patrick.Balkany@gmail.com','0712121212','p.balkany','levallois','employeur','Ingénieur Informatique','25 ans cher google'),
-('Pierre-Antoine','Legger','38','homme','pierre-antoine.legger@gmail.com','0659783241','pa.legger','password','employeur','ingénieur Informatique','10 ans chez snapchat'),
-('Fabien','Bounoir','48','homme','fabien.bounoir@gmail.com','0678415295','','','employeur','ingénieur Informatique','10 ans facebook');
+('Joffrey','Tranchat','20','homme','joffrey.tranchat@epitech.eu','0768785945','j.tranchat','tranchat','demandeur','BTS Bac','stage Grand delta habitat à avignon pendant 6 semaines au services informatique'),
+('Arthur','Mathieu','54','homme','arthur.mathieu@gmail.com','0769696969','a.mathieu','mathieu','demandeur',' Informatique','25 ans en freelance'),
+('Ethan','Villeseche','47','homme','ethan.villeseche@gmail.com','0712121212','e.villeseche','villeseche','employeur','Ingénieur Informatique','25 ans cher google'),
+('Pierre-Antoine','Legger','38','homme','pierre-antoine.legger@gmail.com','0659783241','pa.legger','legger','employeur','ingénieur Informatique','10 ans chez snapchat'),
+('Fabien','Bounoir','48','homme','fabien.bounoir@gmail.com','0678415295','f.bounoir','bounoir','employeur','ingénieur Informatique','10 ans facebook');
 
-INSERT INTO Annonce (nom, description, idEntreprise, place, salaires, tempDeTravailParSemaine, contrat)
+INSERT INTO Annonce (idEntreprise, idPersonne, nom, description, place, salaires, tempDeTravailParSemaine, contrat)
 VALUES
-('Ingénieur informatique','cherche ingénieur informatique','1','1','2500', '35', 'CDI'),
-('dévellopeur web','cherche dévellopeur web Javascript','2','1','2500', '35', 'CDD'),
-('dévellopeur fullstack','cherche dévellopeur fullstack','3','1','2500', '35', 'CDD');
+('1','4','Ingénieur informatique','cherche ingénieur informatique','1','2500', '35', 'CDI'),
+('2','5','dévellopeur web','cherche dévellopeur web Javascript','1','2500', '35', 'CDD'),
+('3','6','dévellopeur fullstack','cherche dévellopeur fullstack','1','2500', '35', 'CDD');
